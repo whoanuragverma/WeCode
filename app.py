4a1ece76022dd9c475286972836b68b2c1b02402
Anurag Verma <anu.anuragverma@gmail.com>
2020-03-26 18:45:45 +0530

    Working database just UI left

--- app.py
+++ app.py
@@ -1,27 +1,69 @@
-from flask import Flask, render_template
-import random
-import string
-
-def randomStringDigits(stringLength=6):
-    lettersAndDigits = string.ascii_letters + string.digits
-    return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
-
+from flask import Flask, render_template,request,redirect,url_for,jsonify,make_response
+from others import randomStringDigits
+from flask_sqlalchemy import SQLAlchemy
+from time import ctime
+from hashlib import md5
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "djXEHjRAPPtzjNQRl7eJ.53/mgQDGWktEechrRhP45Ez"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+class Code(db.Model):
+    ms = db.Column('code_id',db.String(6),primary_key = True)
+    cd = db.Column(db.String(100000))
+    ip = db.Column(db.String(24))
+    time = db.Column(db.String(50))
+
+    def __init__(self,ms,cd,ip,time):
+        self.ms = ms
+        self.cd = cd
+        self.ip = ip
+        self.time = time
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    ID = randomStringDigits() 
+    data = Code(ms=ID,cd="",ip="",time="")
+    db.session.add(data)
+    db.session.commit()
+    response = make_response(redirect(url_for('main',word=ID)))
+    trial = 1
+    while request.cookies.get('ID' + str(trial)) != None:
+        trial += 1 
+    response.set_cookie('ID' + str(trial) ,md5(ID.encode()).hexdigest(),max_age=60*60*24)
+    response.set_cookie('admin','true')
+    response.set_cookie('_gwa',ctime())
+    return response
 
 @app.route('/<word>')
 def main(word):
-    return word
+    trial = 1
+    while (request.cookies.get('ID' + str(trial)) != None) or (request.cookies.get('ID' + str(trial)) == md5(word.encode()).hexdigest()):
+        if request.cookies.get('admin') == 'true':
+            return render_template('show.html',data = Code.query.filter_by(ms=word).first())
+        trial += 1
+    return render_template('disp.html',ajaxdata=word)
 
-@app.route('/add',methods=['GET','POST'])
-def add():
-    return randomStringDigits()
 
+@app.route('/json/<word>')
+def jsonresp(word):
+    data = Code.query.filter_by(ms=word).first()
+    return jsonify({"code":data.cd,"ip":data.ip,"time":data.time})
+
+@app.route('/edit/<word>',methods=['POST'])
+def edit(word):
+    if request.method == 'POST':
+        data = Code.query.filter_by(ms=word).first()
+        db.session.delete(data)
+        db.session.commit()
+        data = Code(ms=word,cd=request.form['code'],ip=request.form['ip'],time=request.form['date'])
+        db.session.add(data)
+        db.session.commit()
+        return jsonify({"response":"saved","time":ctime()})
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
